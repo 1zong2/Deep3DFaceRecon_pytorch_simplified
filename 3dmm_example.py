11b6e5f6bacb20f618fd4cc99d53080dbdb0ea49
1zong2 <1zong2@innerverz.com>
2022-04-15 18:59:15 +0900

    minor fixes

--- 3dmm_example.py
+++ 3dmm_example.py
@@ -22,16 +22,16 @@ if __name__ == "__main__":
     save_root = "result"
     os.makedirs(save_root, exist_ok=True)
 
-    source_fname = "00005.png"
-    target_fname = "00004.png"
-    result_fname = "5to4.png"
+    source_fname = "00000.png"
+    target_fname = "00005.png"
+    result_fname = "0to5.png"
 
     source_load_path = f"{load_root}/{source_fname}"
     target_load_path = f"{load_root}/{target_fname}"
     source_save_path = f"{save_root}/{source_fname}"
     target_save_path = f"{save_root}/{target_fname}"
     result_save_path = f"{save_root}/{result_fname}"
-    combine_save_path = f"{save_root}/{result_fname[:-4]}_combine.png"
+    combine_save_path = f"{save_root}/{result_fname[:-4]}_grid.png"
 
     ### source
     source_img = Image.open(source_load_path).convert('RGB').resize((256, 256))
@@ -55,11 +55,11 @@ if __name__ == "__main__":
 
     ### mix
     mix_coeff = target_coeff.clone()
-    mix_coeff[:, :80] = source_coeff[:, :80] # identity
-    # mix_coeff[:, 80:144] = source_coeff[:, 80:144] # expression 
+    # mix_coeff[:, :80] = source_coeff[:, :80] # identity
+    mix_coeff[:, 80:144] = source_coeff[:, 80:144] # expression 
     # mix_coeff[:, 144:224] = source_coeff[:, 144:224] # texture
-    # mix_coeff[:, 224:227] = source_coeff[:, 224:227] # angles
-    mix_coeff[:, 227:254] = source_coeff[:, 227:254] # gammas
+    mix_coeff[:, 224:227] = source_coeff[:, 224:227] # angles
+    # mix_coeff[:, 227:254] = source_coeff[:, 227:254] # gammas
     # mix_coeff[:, 254:] = source_coeff[:, 254:] # translations
 
     mix_3dface = face_recon_model.coeff_to_3dface(mix_coeff)
